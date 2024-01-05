5a00f0687008afea8191bff09e8fe4e1a301ffb1
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2024-01-05 02:52:05 -0300

    feat: Create Section component

---
+++ src/components/Section/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    width: 100%;
+    > h2 {
+        font-size: 3.2rem;
+        font-weight: 500;
+        line-height: 140%;
+    }
+`
